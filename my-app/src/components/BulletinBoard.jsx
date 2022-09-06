9f75cecadcb2d5f5faca4ca85cbb4c23fd0b005a
fanphi <fannyp90@hotmail.com>
2022-09-06 21:56:17 +0200

    added more info

--- my-app/src/components/BulletinBoard.jsx
+++ my-app/src/components/BulletinBoard.jsx
@@ -9,11 +9,11 @@ const BulletinBoard = () => {
         liked: false,
         id: 1},
         {question: "Vad betyder Single Page Application och vad skiljer det från en “vanlig” webbplats?",
-        answer: "Lorem Ipsum",
+        answer: "När du går in på en vanlig webbplats och klickar på en länk som tar dig till en ny sida, så behöver den sidan laddas om. Med en Single Page Application betyder det att själva sidan endast laddas en gång och bara nödvändig ny information behöver ändras. Detta gör en Single Page Application snabbare än en vanlig hemsida.",
         liked: false,
         id: 2},
         {question: "Nämn tre skillnader mellan React och Angular.",
-        answer: "Lorem Ipsum",
+        answer: "React är ett JavaScript-bibliotek och Angular är ett ramverk. React använder sig av en virtuell DOM och Angular använder sig av en riktig DOM. Det betyder att med Angular så påverkas hela strukturen om endast en liten ändring görs, men med React så går det att göra ändringar utan att hela DOMen behöver uppdateras. Angular är skrivet i Typescript och React är skrivet i JavaScript.",
         liked: false,
         id: 3},
       ]
--- my-app/src/components/PostLiked.jsx
+++ my-app/src/components/PostLiked.jsx
@@ -1,7 +1,7 @@
 import { FaHeart } from "react-icons/fa";
 const PostLiked = () => {
     return(
-        <div>You have liked this post 
+        <div>You have liked this post! 
             <br></br><FaHeart className="heart"/>
         </div>
     )  
